652fdc8f7ec22b354680d4f40f201433774072c4
alcaldia05 <117576633+alcaldia05@users.noreply.github.com>
2022-11-26 09:54:41 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN = '5771140185:AAEXgz_lUQH9mxBYC2U7X3EMM0v_0ro1-iU' #Aqui va el token del bot
+BOT_TOKEN = '5591936959:AAGRXjdg-rZm0vaDKYPpz1EFoiEcyiCf4eY' #Aqui va el token del bot
 API_ID =  13233271 #Tu api id de telegram
 API_HASH = 'e3ce8145aa657c2a4cc5cf0f7183e476' #Tu api id de telegram
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','nautaii').split(';')
 
-static_proxy = 'socks5h://KGGJJJYIJFLIFEYDJFLEKHYGCHDHRHHKCHHGCE'
+static_proxy = 'socks5h://KDDHKIYEJEJGGIYFJEGDIEYEKIKGFFRJHHCDHICJ'
 PROXY = ProxyCloud.parse(static_proxy)
 
 if PROXY:
