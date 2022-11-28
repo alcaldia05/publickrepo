c7837a56e9aa37908dd0986a1e48855dada6ce71
alcaldia05 <117576633+alcaldia05@users.noreply.github.com>
2022-11-28 18:45:34 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','nautaii').split(';')
 
-static_proxy = 'socks5h://KDDHKIYEJEJGGIYFJEGDIEYEKIKGFFRJHHCDHICJ'
+static_proxy = 'socks5h://KEGKJJYEJFLIFHYDKGKGCKYJGJCGRJCELGGHLD'
 PROXY = ProxyCloud.parse(static_proxy)
 
 if PROXY:
